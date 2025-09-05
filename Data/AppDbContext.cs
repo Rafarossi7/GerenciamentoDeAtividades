4ee1aa81ff8259367a34be1caadae14634d5b265
Rafael Rossi <rafaelrossi7@outlook.com>
2025-09-05 15:00:54 -0300

    Adiciona projeto completo com README atualizado

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -27,17 +27,3 @@ namespace GerenciamentoAtividadesApi.Data
 
 
 
-//using Microsoft.EntityFrameworkCore;
-//using GerenciamentoAtividadesApi.Models;
-
-//namespace GerenciamentoAtividadesApi.Data
-//{
-//    public class AppDbContext : DbContext
-//    {
-//        public AppDbContext(DbContextOptions options) : base(options) { }
-
-
-//        public DbSet<Usuario> Usuarios { get; set; }
-
-//    }
-//}
